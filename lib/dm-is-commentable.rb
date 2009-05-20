f1a682e42069c2242837b052a495d76e1a96c877
snusnu <gamsnjaga@gmail.com>
2009-05-20 02:39:08 +0200

    removed usage of rubygems

--- lib/dm-is-commentable.rb
+++ lib/dm-is-commentable.rb
@@ -1,15 +1,4 @@
-# Needed to import datamapper and other gems
-require 'rubygems'
 require 'pathname'
-
-# Add all external dependencies for the plugin here
-gem 'dm-core',          '>=0.9.5'
-gem 'dm-types',         '>=0.9.5'
-gem 'dm-timestamps',    '>=0.9.5'
-gem 'dm-validations',   '>=0.9.5'
-gem 'dm-aggregates',    '>=0.9.5'
-gem 'dm-is-remixable',  '>=0.9.5'
-gem 'dm-is-rateable',   '>=0.0.1'
 
 require 'dm-core'
 require 'dm-types'
